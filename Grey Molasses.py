a69f041b7206cf67b5963b8a89582834daa6e0d6
biganicco <37968040+biganicco@users.noreply.github.com>
2018-04-16 04:33:42 +0200

    Update Grey Molasses.py
    Got rid of the Wigner 6j symbols and went for a more brute-force approach.

--- Grey Molasses.py
+++ Grey Molasses.py
@@ -8,21 +8,20 @@ Last edited: 2 April 2018
 Last edited by: Niccolò 
 """
 
+# -*- coding: utf-8 -*-
+"""
+Grey Molasses Simulation
+
+Will Lab, Columbia University Physics Department
+
+Date created: 27 March 2018
+Last edited: 2 April 2018
+Last edited by: Niccolò 
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
-#import sympy 
-from sympy import S
-from sympy.physics.wigner import clebsch_gordan as cg
-from sympy.physics.quantum.cg import Wigner3j
-from sympy.physics.quantum.cg import Wigner6j
-'''
-from sympy.physics.wigner import wigner_3j as w3j
-from sympy.physics.wigner import wigner_3j as w6j
-'''
-'''
-Initializations
-'''
+
 #Initialize the molasses states
 Fdark = 1
 Fbright = 2
@@ -35,11 +34,7 @@ m_Fdark = 2*Fdark + 1
 m_Fbright = 2*Fbright + 1
 m_Fexcited = 2*Fexcited + 1
 StatesNumber = m_Fdark + m_Fbright + m_Fexcited
-#JRedDip = 3.5247 #Value of <J||e\vec{r}||J'> for the D2 line - from Steck units of [e*a_0]
-'''
-A = Wigner3j(6, 0, 4, 0, 2, 0).doit()
-print(float(A)+0.34)
-'''
+
 #Initialize an array containing the state labels for states of the form |J,F,I,m_F>
 StLab = np.zeros((4,StatesNumber)) 
 for i in range(0,m_Fdark):
@@ -55,88 +50,87 @@ for i in range(m_Fdark+m_Fbright,StatesNumber):
     StLab[1,i] = Fexcited
     StLab[2,i] = -Fexcited-(m_Fdark+m_Fbright)+i
     
-#Initialize lasers parameters
-#wc = #cooling laser frequency [GHz]
-#wr = #repumper laser frequency [GHz]
-
 #Initialize the unperturbed energies [GHz] - zero set at lowest energy
 Ed = 0                                                      #energy of the F=1 manifold 
 Eb = 6.83468261090429                                       #energy of the F=2 manifold 
 Ee = 4.27167663181519 + 384.2304844685*10**3 - 72.9113*10**(-3)    #energy of the F'=2 manifold 
 
+#Initialize lasers parameters
 DeltaRC = 2*np.pi*6834.6*10**(-3)
 gamma = 2*np.pi*6.065*10**(-3)
 omega_R = 5*gamma+Ee
 RabiR = 1.2*gamma
 RabiC = 4.2*gamma
 
-Omega = np.matrix([[1,0,0,0,0,0,0,0,RabiR,RabiR,RabiR,RabiR,RabiR],
-         [0,1,0,0,0,0,0,0,RabiR,RabiR,RabiR,RabiR,RabiR],
-         [0,0,1,0,0,0,0,0,RabiR,RabiR,RabiR,RabiR,RabiR],
-         [0,0,0,1,0,0,0,0,RabiC,RabiC,RabiC,RabiC,RabiC],
-         [0,0,0,0,1,0,0,0,RabiC,RabiC,RabiC,RabiC,RabiC],
-         [0,0,0,0,0,1,0,0,RabiC,RabiC,RabiC,RabiC,RabiC],
-         [0,0,0,0,0,0,1,0,RabiC,RabiC,RabiC,RabiC,RabiC],
-         [0,0,0,0,0,0,0,1,RabiC,RabiC,RabiC,RabiC,RabiC],
-         [RabiR,RabiR,RabiR,RabiC,RabiC,RabiC,RabiC,RabiC,1,0,0,0,0],
-         [RabiR,RabiR,RabiR,RabiC,RabiC,RabiC,RabiC,RabiC,0,1,0,0,0],
-         [RabiR,RabiR,RabiR,RabiC,RabiC,RabiC,RabiC,RabiC,0,0,1,0,0],
-         [RabiR,RabiR,RabiR,RabiC,RabiC,RabiC,RabiC,RabiC,0,0,0,1,0],
-         [RabiR,RabiR,RabiR,RabiC,RabiC,RabiC,RabiC,RabiC,0,0,0,0,1]])
-
 #Hamiltonians
 H0 = np.matrix(np.zeros((StatesNumber,StatesNumber))) #Initialize the free Hamiltonian
 for i in range(0,m_Fdark-1):
     H0[i,i]=Ed
 for i in range(m_Fdark,m_Fdark+m_Fbright):
-    H0[i,i]=Eb
+    H0[i,i]=Eb-DeltaRC
 for i in range(m_Fdark+m_Fbright,StatesNumber):
-    H0[i,i]=Ee
-    
-q = 1
-Hp1 = np.matrix(np.zeros((StatesNumber,StatesNumber))) #Initialize the interaction Hamiltonian
-for i in range(0,StatesNumber-1):
-    for j in range(0,StatesNumber-1):
-        A = (-1)**(StLab[1,j]-1+StLab[2,i])*(2*StLab[1,i]+1)**0.5*float(Wigner3j(StLab[1,j], 1, StLab[1,i], StLab[2,j], q, -StLab[2,i]).doit())
-        B = (-1)**(StLab[1,j]+StLab[0,i]+1+float(INa))*((2*StLab[1,j]+1)*(2*StLab[0,i]))**0.5*float(Wigner6j(StLab[0,i], StLab[0,j], 1, StLab[1,j], StLab[1,i], INa).doit())
-        Hp1[i,j]= A*B*Omega[i,j]
+    H0[i,i]=Ee-omega_R
+H = np.matrix(np.zeros((StatesNumber,StatesNumber)))
+Hp1 = np.matrix(np.zeros((StatesNumber,StatesNumber))) #Initialize the interaction Hamiltonian with \sigma_+ light
+Hp0 = np.matrix(np.zeros((StatesNumber,StatesNumber))) #Initialize the interaction Hamiltonian with \pi light
+Hm1 = np.matrix(np.zeros((StatesNumber,StatesNumber))) #Initialize the interaction Hamiltonian with \sigma_- light
 
-q = 0
-Hp0 = np.matrix(np.zeros((StatesNumber,StatesNumber))) #Initialize the interaction Hamiltonian
-for i in range(0,StatesNumber-1):
-    for j in range(0,StatesNumber-1):
-        A = (-1)**(StLab[1,j]-1+StLab[2,i])*(2*StLab[1,i]+1)**0.5*float(Wigner3j(StLab[1,j], 1, StLab[1,i], StLab[2,j], q, -StLab[2,i]).doit())
-        B = (-1)**(StLab[1,j]+StLab[0,i]+1+float(INa))*((2*StLab[1,j]+1)*(2*StLab[0,i]))**0.5*float(Wigner6j(StLab[0,i], StLab[0,j], 1, StLab[1,j], StLab[1,i], INa).doit())
-        Hp0[i,j] = A*B*Omega[i,j]
-        
-q = -1
-Hm1 = np.matrix(np.zeros((StatesNumber,StatesNumber))) #Initialize the interaction Hamiltonian
-for i in range(0,StatesNumber-1):
-    for j in range(0,StatesNumber-1):
-        A = (-1)**(StLab[1,j]-1+StLab[2,i])*(2*StLab[1,i]+1)**0.5*float(Wigner3j(StLab[1,j], 1, StLab[1,i], StLab[2,j], q, -StLab[2,i]).doit())
-        B = (-1)**(StLab[1,j]+StLab[0,i]+1+float(INa))*((2*StLab[1,j]+1)*(2*StLab[0,i]))**0.5*float(Wigner6j(StLab[0,i], StLab[0,j], 1, StLab[1,j], StLab[1,i], INa).doit())
-        Hm1[i,j]= A*B*Omega[i,j]
+#Brute force interaction matrix elements
+Hm1[0,8] = 4**(-0.5)*RabiR
+Hm1[1,9] = 8**(-0.5)*RabiR 
+Hm1[2,10] = 24**(-0.5)*RabiR 
+Hm1[4,8] = -12**(-0.5)*RabiC 
+Hm1[5,9] = -8**(-0.5)*RabiC
+Hm1[6,10] = -8**(-0.5)*RabiC 
+Hm1[7,11] = -12**(-0.5)*RabiC
+Hm1[8,0] = 4**(-0.5)*RabiR 
+Hm1[9,1] = 8**(-0.5)*RabiR 
+Hm1[10,2] = 24**(-0.5)*RabiR 
+Hm1[8,4] = -12**(-0.5)*RabiC 
+Hm1[9,5] = -8**(-0.5)*RabiC 
+Hm1[10,6] = -8**(-0.5)*RabiC
+Hm1[11,7] = -12**(-0.5)*RabiC
 
-H = np.matrix(np.zeros((StatesNumber,StatesNumber)),dtype=np.complex)
+Hp1[0,10] = 24**(-0.5)*RabiR
+Hp1[1,11] = 8**(-0.5)*RabiR
+Hp1[2,12] = 4**(-0.5)*RabiR
+Hp1[3,9] = 12**(-0.5)*RabiC
+Hp1[4,10] = 8**(-0.5)*RabiC
+Hp1[5,11] = 8**(-0.5)*RabiC
+Hp1[6,12] = 12**(-0.5)*RabiC
+Hp1[10,0] = 24**(-0.5)*RabiR
+Hp1[11,1] = 8**(-0.5)*RabiR
+Hp1[12,2] = 4**(-0.5)*RabiR
+Hp1[9,3] = 12**(-0.5)*RabiC
+Hp1[10,4] = 8**(-0.5)*RabiC
+Hp1[11,5] = 8**(-0.5)*RabiC
+Hp1[12,6] = 12**(-0.5)*RabiC
+
+Hp0[0,9] = -8**(-0.5)*RabiR
+Hp0[1,10] = -6**(-0.5)*RabiR
+Hp0[2,11] = -8**(-0.5)*RabiR
+Hp0[3,8] = -6**(-0.5)*RabiC
+Hp0[4,9] = -24**(-0.5)*RabiC
+Hp0[5,10] = 0*RabiC
+Hp0[6,11] = 24**(-0.5)*RabiC
+Hp0[7,12] = 6**(-0.5)*RabiC
+Hp0[9,0] = -8**(-0.5)*RabiR
+Hp0[10,1] = -6**(-0.5)*RabiR
+Hp0[11,2] = -8**(-0.5)*RabiR
+Hp0[8,3] = -6**(-0.5)*RabiC
+Hp0[9,4] = -24**(-0.5)*RabiC
+Hp0[10,5] = 0*RabiC
+Hp0[11,6] = 24**(-0.5)*RabiC
+Hp0[12,7] = 6**(-0.5)*RabiC
+
+
 DeltaKZ = 0.001
 steps = int(np.int(np.pi/(2*DeltaKZ)))
 KZ = np.zeros((steps,))
 for i in range(0,steps-1):
     KZ[i] = i*DeltaKZ
-'''
-k= np.pi/4
-for i in range(0,StatesNumber-1):
-    for j in range(0,StatesNumber-1):
-        if i == j:
-            H[i,j] = H0[i,j] + Hp1[i,j] + Hm1[i,j] + Hp0[i,j]
-        elif i<j:
-            H[i,j] = H0[i,j] + 2**0.5*np.cos(k)*Hp1[i,j]+1j*2**0.5*np.sin(k)*Hm1[i,j]+(np.cos(k)*(1-1j)+np.sin(k)*(1+1j))*Hp0[i,j]
-        elif j<i:
-            H[i,j] = H0[i,j] + 2**0.5*np.cos(k)*Hp1[i,j]-1j*2**0.5*np.sin(k)*Hm1[i,j]+(np.cos(k)*(1+j)+np.sin(k)*(1-1j))*Hp0[i,j]
-Energies, holder = LA.eig(H)
-'''
-Energies = np.array(np.zeros((13,steps)))
-LA.eig(np.diag((1, 2, 3)))
+
+Energies = np.zeros((StatesNumber,steps))
 for t in range(0,steps-1):
     k = np.pi*t/steps
     for i in range(0,StatesNumber-1):
@@ -147,7 +141,9 @@ for t in range(0,steps-1):
                 H[i,j] = H0[i,j] + 2**0.5*np.cos(k)*Hp1[i,j]+1j*2**0.5*np.sin(k)*Hm1[i,j]+(np.cos(k)*(1-1j)+np.sin(k)*(1+1j))*Hp0[i,j]
             elif j>i:
                 H[i,j] = H0[i,j] + 2**0.5*np.cos(k)*Hp1[i,j]-1j*2**0.5*np.sin(k)*Hm1[i,j]+(np.cos(k)*(1+j)+np.sin(k)*(1-1j))*Hp0[i,j]
-    Energies[:,i], holder = LA.eig(H)
+    Energies[:,t], holder = np.linalg.eigh(H)
+    H = np.matrix(np.zeros((StatesNumber,StatesNumber)))
 
-plt.plot(KZ,Energies[1,:])
+plt.plot(KZ,Energies[0,:],'r.',KZ,Energies[1,:],'r.',KZ,Energies[2,:],'r.',KZ,Energies[3,:],'r.',KZ,Energies[4,:],'r.',KZ,Energies[5,:],'r.',KZ,Energies[6,:],'r.',KZ,Energies[7,:],'r.',KZ,Energies[8,:],'r.',KZ,Energies[9,:],'r.',KZ,Energies[10,:],'r.',KZ,Energies[11,:],'r.',KZ,Energies[12,:],'r.')
+plt.ylim([-36.1088,-36.1083])
 plt.show()
