2f3f2dd44bd056356ab1a92b9b1c3e87ae96aa1f
biganicco <37968040+biganicco@users.noreply.github.com>
2018-04-19 14:29:14 +0200

    Update Grey Molasses.py
    Changed the polarization part

--- Grey Molasses.py
+++ Grey Molasses.py
@@ -1,13 +1,3 @@
-"""
-Grey Molasses Simulation
-
-Will Lab, Columbia University Physics Department
-
-Date created: 27 March 2018
-Last edited: 2 April 2018
-Last edited by: Niccolò 
-"""
-
 # -*- coding: utf-8 -*-
 """
 Grey Molasses Simulation
@@ -15,21 +5,20 @@ Grey Molasses Simulation
 Will Lab, Columbia University Physics Department
 
 Date created: 27 March 2018
-Last edited: 2 April 2018
+Last edited: 18 April 2018
 Last edited by: Niccolò 
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy import linalg as LA
 
 #Initialize the molasses states
 Fdark = 1
 Fbright = 2
 Fexcited = 2
-Jdark = S(1)/2
+Jdark = 1/2
 Jbright = Jdark
-Jexcited = S(3)/2
-INa = S(3)/2
+Jexcited = 3/2
+INa = 3/2
 m_Fdark = 2*Fdark + 1
 m_Fbright = 2*Fbright + 1
 m_Fexcited = 2*Fexcited + 1
@@ -59,6 +48,9 @@ Ee = 4.27167663181519 + 384.2304844685*10**3 - 72.9113*10**(-3)    #energy of th
 DeltaRC = 2*np.pi*6834.6*10**(-3)
 gamma = 2*np.pi*6.065*10**(-3)
 omega_R = 5*gamma+Ee
+#RabiR = 1.2*gamma
+#RabiC = 4.2*gamma
+
 RabiR = 1.2*gamma
 RabiC = 4.2*gamma
 
@@ -80,15 +72,15 @@ Hm1[0,8] = 4**(-0.5)*RabiR
 Hm1[1,9] = 8**(-0.5)*RabiR 
 Hm1[2,10] = 24**(-0.5)*RabiR 
 Hm1[4,8] = -12**(-0.5)*RabiC 
-Hm1[5,9] = -8**(-0.5)*RabiC
+Hm1[5,9] = -8**(-0.5)*RabiC 
 Hm1[6,10] = -8**(-0.5)*RabiC 
-Hm1[7,11] = -12**(-0.5)*RabiC
+Hm1[7,11] = -12**(-0.5)*RabiC 
 Hm1[8,0] = 4**(-0.5)*RabiR 
 Hm1[9,1] = 8**(-0.5)*RabiR 
 Hm1[10,2] = 24**(-0.5)*RabiR 
 Hm1[8,4] = -12**(-0.5)*RabiC 
 Hm1[9,5] = -8**(-0.5)*RabiC 
-Hm1[10,6] = -8**(-0.5)*RabiC
+Hm1[10,6] = -8**(-0.5)*RabiC 
 Hm1[11,7] = -12**(-0.5)*RabiC
 
 Hp1[0,10] = 24**(-0.5)*RabiR
@@ -123,27 +115,29 @@ Hp0[10,5] = 0*RabiC
 Hp0[11,6] = 24**(-0.5)*RabiC
 Hp0[12,7] = 6**(-0.5)*RabiC
 
-
 DeltaKZ = 0.001
-steps = int(np.int(np.pi/(2*DeltaKZ)))
+Limit = np.pi
+steps = np.int(Limit/(DeltaKZ))
 KZ = np.zeros((steps,))
 for i in range(0,steps-1):
     KZ[i] = i*DeltaKZ
 
 Energies = np.zeros((StatesNumber,steps))
 for t in range(0,steps-1):
-    k = np.pi*t/steps
+    k = Limit*t/steps
     for i in range(0,StatesNumber-1):
         for j in range(0,StatesNumber-1):
             if i == j:
                 H[i,j] = H0[i,j] + Hp1[i,j] + Hm1[i,j] + Hp0[i,j]
             elif i>j:
-                H[i,j] = H0[i,j] + 2**0.5*np.cos(k)*Hp1[i,j]+1j*2**0.5*np.sin(k)*Hm1[i,j]+(np.cos(k)*(1-1j)+np.sin(k)*(1+1j))*Hp0[i,j]
-            elif j>i:
-                H[i,j] = H0[i,j] + 2**0.5*np.cos(k)*Hp1[i,j]-1j*2**0.5*np.sin(k)*Hm1[i,j]+(np.cos(k)*(1+j)+np.sin(k)*(1-1j))*Hp0[i,j]
+                H[i,j] = H0[i,j] + np.exp(1j*np.pi/4)*(Hp1[i,j]*(np.cos(k)-np.sin(k))+1j*Hm1[i,j]*(np.cos(k)+np.sin(k)))
+            elif i<j:
+                H[i,j] = H0[i,j] + np.exp(-1j*np.pi/4)*(Hp1[i,j]*(np.cos(k)-np.sin(k))-1j*Hm1[i,j]*(np.cos(k)+np.sin(k)))
     Energies[:,t], holder = np.linalg.eigh(H)
     H = np.matrix(np.zeros((StatesNumber,StatesNumber)))
-
+    
 plt.plot(KZ,Energies[0,:],'r.',KZ,Energies[1,:],'r.',KZ,Energies[2,:],'r.',KZ,Energies[3,:],'r.',KZ,Energies[4,:],'r.',KZ,Energies[5,:],'r.',KZ,Energies[6,:],'r.',KZ,Energies[7,:],'r.',KZ,Energies[8,:],'r.',KZ,Energies[9,:],'r.',KZ,Energies[10,:],'r.',KZ,Energies[11,:],'r.',KZ,Energies[12,:],'r.')
-plt.ylim([-36.1088,-36.1083])
+#plt.ylim([-36.10855,-36.10835])
+plt.ylim([-0.001,0.003])
+#plt.ylim([0.001,0.0015])
 plt.show()
